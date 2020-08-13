207b1e132b8546495e6e032f2213701876a517da
Ryan-Rguo <68999754+Ryan-Rguo@users.noreply.github.com>
2020-08-13 16:57:26 +1000

    Update Chapter 7_Project 1.py

--- Chapter 7_Project 1.py
+++ Chapter 7_Project 1.py
@@ -1,10 +1,11 @@
 # Chaptor 7
 
 # Practice Project 1
+# Here are two functions that return the same result. 
 
 import re
 
-def pswTest():
+def pswTestLong():
   while True:
      print('Please input a password that contains at least 8 letters includs number, lowercase, and uppercase.')
      pw = input()
@@ -20,10 +21,31 @@ def pswTest():
                 dig = re.compile(r'[0-9]')
 
                 if dig.search(pw).group() != None:
-                   print('Great job, this is a strong password!')
-                   break
-     except:
-          print('The password is too weak! Try again...')
-     
-pswTest()        # Remove the # to start testing.
+                   special = re.compile(r'^((?!\W).)*$')
 
+                   if special.search(pw).group() != None:
+                       print('Great job, this is a strong password!')
+                       break
+     except:
+          print('The password is invalid! Try again...')
+     
+    
+    
+def pswTestShort():
+  while True:
+     print('Please input a password that contains at least 8 characters includ number, lowercase, and uppercase.')
+     pw = input()
+     num = re.compile(r'''
+          (?=.*\d)                 # At least 1 number
+          (?=.*[a-z])              # At least 1 lowercase letter
+          (?=.*[A-Z])              # At least 1 uppercase letter
+          (?=^((?!\W).)*$)         # No special characters
+          ([0-9a-zA-Z]{8,}         # At least 8 characters
+          )''',re.VERBOSE)
+     try:
+          if num.search(pw).group() is not None:
+               print('Great job, this is a strong password!')
+               break
+     except:
+          print('The password is invalid! Try again...')
+    
